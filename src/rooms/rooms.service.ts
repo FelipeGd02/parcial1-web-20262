23f9ed123ca0bcb45ec32ac52a9c74d936762049
Felipe Gonzalez Delgado <1105366706@u.icesi.edu.co>
2026-09-07 20:14:42 -0500

    eso es todo

--- src/rooms/rooms.service.ts
+++ src/rooms/rooms.service.ts
@@ -1,18 +1,75 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable,  NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { RoomEntity } from './entities/room.entity';
 import { Repository } from 'typeorm/browser';
 import { CreateRoomDto } from './dto/create-room.dto';
+import { ScreeningEntity } from '../screenings/entities/screening.entity';
+
 
 @Injectable()
 export class RoomsService {
-    constructor (
-        @InjectRepository (RoomEntity)
-        private readonly roomsRepository: Repository <RoomEntity>,
-    ){}
-    async create (data: CreateRoomDto): Promise <RoomEntity> {
-        const room = this.roomsRepository.create(data);
-        return this.roomsRepository.save (room);
+
+  constructor(
+    @InjectRepository(RoomEntity)
+    private readonly roomsRepository: Repository<RoomEntity>,
+
+    @InjectRepository(ScreeningEntity)
+    private readonly screeningsRepository: Repository<ScreeningEntity>,) {}
+
+  async create(createRoomDto: CreateRoomDto): Promise<RoomEntity> {
+    const room = await this.roomsRepository.findOneBy({
+      id: createRoomDto.roomId,
+    });
+
+    if (!screening) {
+      throw new NotFoundException(
+        `Screening with id ${createRoomDto.roomId} was not found`,
+      );
     }
-    async findAll
+
+    const room = this.roomsRepository.create({
+      name: createRoomDto.name,
+      capacity: createRoomDto.capacity,
+      screening,
+    });
+
+    return this.roomsRepository.save(room);
+  }
+
+  async findAll(): Promise<RoomEntity[]> {
+    return this.roomsRepository.find({
+      relations: {
+        screening: true,
+      },
+      room: {
+        id: "ASC",
+      },
+    });
+  }
+
+  async findOne(id: number): Promise<RoomEntity> {
+    const room = await this.roomsRepository.findOne({
+      where: { id },
+      relations: {
+        screening: true,
+      },
+    });
+
+    if (!room) {
+      throw new NotFoundException(`Room with id ${id} was not found`);
+    }
+
+    return room;
+  }
+
+  async update(
+    id: number,
+    updateRoomDto: UpdateRoomDto,
+  ): Promise<RoomEntity> {
+    const room = await this.findOne(id);
+
+    this.roomsRepository.merge(room, updateRoomDto);
+
+    return this.roomsRepository.save(room);
+  }
 }
--- src/screenings/screenings.service.ts
+++ src/screenings/screenings.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ScreeningEntity } from './entities/screening.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ScreeningsService {}
+export class ScreeningsService {
+    constructor (
+        @InjectRepository (ScreeningEntity)
+        private readonly screeningsRepository: Repository<ScreeningEntity>,
+    ){}
+    async create (data:)
+}
